d288322ae0186e6ebeadf0a2dbe8c6319b22bad6
Rafiul29 <rafiulislam4472@gmail.com>
2024-08-18 11:51:51 +0600

    email url chnage

--- account/views.py
+++ account/views.py
@@ -29,7 +29,7 @@ class UserRegistrationView(APIView):
             user = serializer.save()
             token=default_token_generator.make_token(user)
             uid=urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link=f'https://rent-vista-7tlr.onrender.com/api/auth/active/{uid}/{token}/'
+            confirm_link=f'https://rent-vista.onrender.com/api/auth/active/{uid}/{token}/'
             # confirm_link=f'http://127.0.0.1:8000/api/auth/active/{uid}/{token}/'
             email_subject='Confirm Your Email'
             email_body=render_to_string('confirm_email.html',{'confirm_link':confirm_link})
